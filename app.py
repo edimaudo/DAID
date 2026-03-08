9cb7b4065c9c92fe17ac900a91668ba5499f023e
Edima <edimaudo@gmail.com>
2026-03-08 13:54:47 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse, JSONResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
-from agents import agent_framer ,agent_solution_designer
+from agents import agent_framer, agent_solution_designer
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
